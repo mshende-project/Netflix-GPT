aad376a23b7417b66150d4b618d3c77dce96038c
mshende-project <52695958+mshende-project@users.noreply.github.com>
2023-09-29 16:59:46 +0200

    Delete netflix-gpt directory

--- netflix-gpt/src/utils/openai.js
+++
@@ -1,9 +0,0 @@
-import { OPEN_AI_SECRET_KEY } from "./constants";
-import OpenAI from "openai";
-
-const openai = new OpenAI({
-  apiKey: { OPEN_AI_SECRET_KEY },
-  dangerouslyAllowBrowser: true,
-});
-
-export default openai;
